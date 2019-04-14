29ac20a138e9458502ea738027c02a3e40b473ba
Tiago <tiago4k@gmail.com>
2019-04-14 22:23:05 +0100

    Gets user inputs and verifies chain

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,80 @@
 blockchain = []
+quit_app = False
 
 
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     return float(input('Your transaction amount please: '))
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
-
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+def get_user_choice():
+    return input('Your choice: ')
 
 
-print(blockchain)
+def print_block_elements():
+    print('Outputting Block...')
+    for block in blockchain:
+        print(block)
 
+
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
+
+
+
+while quit_app == False:
+    print('Please choose:')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain')
+    print('3: Manipulate chain')
+    print('4: Exit application')
+    user_choice = get_user_choice()
+
+    if user_choice == '1':
+
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+
+    elif user_choice == '2':
+
+        print_block_elements()
+
+    elif user_choice == '3':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice == '4':
+
+        print('Quitting Application')
+        quit_app = True
+
+    else:
+        print('Invalid Choice')
+    
+    if not verify_chain():
+        print('Invalid Blockchain!')
+        break
+
+print('Done.')
