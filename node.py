18438d5ad53db98d54681980d5904fadab0c0ffe
Tiago <tiago4k@gmail.com>
2019-05-06 21:19:30 +0100

    add, remove, view nodes

--- node.py
+++ node.py
@@ -10,8 +10,14 @@ blockchain = Blockchain(wallet.public_key)
 
 
 @app.route('/', methods=['GET'])
-def get_ui():
+def get_node_ui():
     return send_from_directory('ui', 'node.html')
+
+
+@app.route('/network', methods=['GET'])
+def get_network_ui():
+    return send_from_directory('ui', 'network.html')
+
 
 
 @app.route('/wallet', methods=['POST'])
@@ -154,6 +160,55 @@ def get_chain():
         dict_block['transactions'] = [
             tx.__dict__ for tx in dict_block['transactions']]
     return jsonify(dict_chain), 200
+
+
+@app.route('/node', methods=['POST'])
+def add_node():
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'No data attached'
+        }
+        return jsonify(response), 400
+    if 'nodes' not in values:
+        response = {
+            'message': 'No node data found'
+        }
+        return jsonify(response), 400
+
+    node = values['node']
+    blockchain.add_peer_node(node)
+
+    response = {
+        'message': 'Node Added Successfully.',
+        'all_nodes': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 201
+
+
+@app.route('/node/<node_url>', methods=['DELETE'])
+def remove_node(node_url):
+    if node_url == '' or node_url == None:
+        response = {
+            'message': 'No node found'
+        }
+        return jsonify(response), 400
+
+    blockchain.remove_peer_node(node_url)
+    response = {
+        'message': 'Node Removed Successfully.',
+        'all_nodes': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes', methods=['GET'])
+def get_nodes():
+
+    response = {
+        'all_nodes': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
